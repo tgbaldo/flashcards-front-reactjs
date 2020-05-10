4616c74f931464afe245ddb1deb60b558b7e2c4a
Tiago Baldo <tgbaldo@gmail.com>
2020-05-10 01:12:18 -0300

    add button in packs page

--- src/pages/Packs/index.js
+++ src/pages/Packs/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Button, Space } from 'antd';
 
 export default function PacksPage() {
     return (
         <Row gutter={20}>
-            <Col>
+            <Col span={12}>
                 <h1>Packs<p>Nesta página você encontra seus packs de Flash Cards</p></h1>
+            </Col>
+            <Col span={12} style={{ textAlign: 'right' }}>
+                <Button size="large" type="primary">Novo Pack</Button>
             </Col>
         </Row>
     )
